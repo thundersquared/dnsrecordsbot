504ab9958bab970bfdeede18cb677178a2843515
Anatoli Nicolae <nicolaeanatoli@gmail.com>
2017-11-03 17:35:18 +0100

    Fixed /start, added commands and better checks.

--- bot.go
+++ bot.go
@@ -3,6 +3,7 @@ package main
 import (
   "log"
   "os"
+  "regexp"
   "strings"
 
   "./dns"
@@ -24,6 +25,10 @@ func main() {
   u := tgbotapi.NewUpdate(0)
   u.Timeout = 60
 
+  command := regexp.MustCompile("/(start|help)")
+  domain := regexp.MustCompile("(https?:?/?/?)?[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9].[a-zA-Z]{2,}(.*)")
+  domainType := regexp.MustCompile("(https?:?/?/?)?[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9].[a-zA-Z]{2,}(.*) (.*)")
+
   updates, err := bot.GetUpdatesChan(u)
 
   for update := range updates {
@@ -35,18 +40,43 @@ func main() {
       log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
     }
 
-    // Fetch dig data
-    d := dns.Dns(update.Message.Text)
-    records := d.GetRecords()
+    if (command.MatchString(update.Message.Text)) {
+      var helper = []string {
+        "Hey! I'm the DNS Records Bot.\n",
+        "Use /start or /help to see this message.\n",
+        "To check a domain's records, send the domain name.",
+        "Example: thundersquared.com\n",
+        "To check specific records send the domain name followed by the record type.",
+        "Example: thundersquared.com A",
+      }
 
-    /*for _, element := range records {
-      fmt.Printf("%s\n", element)
-    }*/
+      msg := tgbotapi.NewMessage(update.Message.Chat.ID, strings.Join(helper, "\n"))
+      msg.ReplyToMessageID = update.Message.MessageID
 
-    // Join and reply
-    msg := tgbotapi.NewMessage(update.Message.Chat.ID, strings.Join(records, "\n"))
-    msg.ReplyToMessageID = update.Message.MessageID
+      bot.Send(msg)
+    }
+    
+    if (domain.MatchString(update.Message.Text)) {
+      // Fetch dig data
+      d := dns.Dns(update.Message.Text)
+      records := d.GetRecords()
 
-    bot.Send(msg)
+      /*for _, element := range records {
+        fmt.Printf("%s\n", element)
+      }*/
+
+      // Join and reply
+      msg := tgbotapi.NewMessage(update.Message.Chat.ID, strings.Join(records, "\n"))
+      msg.ReplyToMessageID = update.Message.MessageID
+
+      bot.Send(msg)
+    }
+    
+    if (domainType.MatchString(update.Message.Text)) {
+      msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Work in progress!")
+      msg.ReplyToMessageID = update.Message.MessageID
+
+      bot.Send(msg)
+    }
   }
 }
